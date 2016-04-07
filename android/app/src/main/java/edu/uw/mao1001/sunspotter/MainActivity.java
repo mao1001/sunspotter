f40c94c4f728f4fb7f17e6c2ed8d48e7c7eb7b21
C Nicholas Mao <mao@tacktechnologies.com>
2016-04-07 14:52:21 -0700

    Streamlined some layouts

--- android/app/src/main/java/edu/uw/mao1001/sunspotter/MainActivity.java
+++ android/app/src/main/java/edu/uw/mao1001/sunspotter/MainActivity.java
@@ -2,6 +2,8 @@ package edu.uw.mao1001.sunspotter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.search_bundle);
+
+
+//        LayoutInflater inflator = getLayoutInflater();
+//        View view = inflator.inflate(R.layout.search_bundle, null);
+
     }
 }
