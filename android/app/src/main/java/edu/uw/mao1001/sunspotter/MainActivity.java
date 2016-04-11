b5f91b4411c2485fedef61b37b1b969dd8403990
C Nicholas Mao <mao@tacktechnologies.com>
2016-04-10 20:41:30 -0700

    stub test

--- android/app/src/main/java/edu/uw/mao1001/sunspotter/MainActivity.java
+++ android/app/src/main/java/edu/uw/mao1001/sunspotter/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewStub;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_bundle);
+        setContentView(R.layout.activity_main);
+
+        ViewStub stub = (ViewStub)findViewById(R.id.stub);
+
+        if(stub != null){
+            stub.inflate();
+        }
 
 
 //        LayoutInflater inflator = getLayoutInflater();
